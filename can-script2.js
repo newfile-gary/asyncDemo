8413edc8adf430905171c1263ae82f09690ac4f3
newfile-gary <63457055+newfile-gary@users.noreply.github.com>
2020-04-30 19:03:57 +0800

    Update can-script2.js

--- can-script2.js
+++ can-script2.js
@@ -17,36 +17,22 @@ promise.then(
 
 function initial(products){
     for(let i=0;i<products.length;i++){
-        fetchBlob(products[i]);
-       
+
+        let section=document.createElement("section");
+        let h2=document.createElement("h2");
+        let p=document.createElement("p");
+        let img=document.createElement("img");
+        section.setAttribute("class",products[i].type);
+        h2.textContent=products[i].name;
+        p.textContent="$"+products[i].price;
+        img.src="images/"+products[i].image;
+        img.setAttribute("alt",products[i].name);
+        section.appendChild(h2);
+        section.appendChild(p);
+        section.appendChild(img);
+        main.appendChild(section);
     }
 
-}
-function fetchBlob(product){
-    let promise=fetch("images/"+product.image);
-    promise.then(function(response){
-        return response.blob();
-    }).then(
-        function(blob){
-            let url=URL.createObjectURL(blob);
-            showImg(url,product);
-        }
-    )
-}
-function showImg(url,product){
-    let section=document.createElement("section");
-    let h2=document.createElement("h2");
-    let p=document.createElement("p");
-    let img=document.createElement("img");
-    section.setAttribute("class",product.type);
-    h2.textContent=product.name;
-    p.textContent="$"+product.price;
-    img.src=url;
-    img.setAttribute("alt",product.name);
-    section.appendChild(h2);
-    section.appendChild(p);
-    section.appendChild(img);
-    main.appendChild(section);
 }
 
 let form=document.querySelector("form");
