f98bec3f29dadaab2a7cd58515c5fe98d1cf60f0
newfile-gary <63457055+newfile-gary@users.noreply.github.com>
2020-04-30 16:31:00 +0800

    Update can-script2.js

--- can-script2.js
+++ can-script2.js
@@ -1,11 +1,12 @@
 var main=document.querySelector("main");
 var promise=fetch("products.json");
+var products={};
 promise.then(
     function(response){
         return response.json();
 }).then(
     function(json){
-        let products=json;
+        products=json;
         initial(products);
     }
 ).catch(
@@ -16,22 +17,35 @@ promise.then(
 
 function initial(products){
     for(let i=0;i<products.length;i++){
+        fetchBlob(products[i]);
+        
+    }
 
-        let section=document.createElement("section");
+}
+function fetchBlob(products){
+    let promise=fetch("images/"+products.image);
+    promise.then(function(response){
+        return response.blob();
+    }).then(
+        function(blob){
+            showImg(blob,products);
+        }
+    )
+}
+function showImg(url,products){
+    let section=document.createElement("section");
         let h2=document.createElement("h2");
         let p=document.createElement("p");
         let img=document.createElement("img");
-        section.setAttribute("class",products[i].type);
-        h2.textContent=products[i].name;
-        p.textContent="$"+products[i].price;
-        img.src="images/"+products[i].image;
-        img.setAttribute("alt",products[i].name);
+        section.setAttribute("class",products.type);
+        h2.textContent=products.name;
+        p.textContent="$"+products.price;
+        img.src=url;
+        img.setAttribute("alt",products.name);
         section.appendChild(h2);
         section.appendChild(p);
         section.appendChild(img);
         main.appendChild(section);
-    }
-
 }
 
 let form=document.querySelector("form");
@@ -42,7 +56,7 @@ button.addEventListener("click", showSelectImg);
 
 function showSelectImg(){
     event.preventDefault();
-    console.log("kkk");
+    main.innerHTML="";
     let key=select.value;
     for(let i=0;i<products.length;i++){
         if(key===products[i].type){
