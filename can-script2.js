410511122158713fe5c3761b49317c462721b9b8
newfile-gary <63457055+newfile-gary@users.noreply.github.com>
2020-05-02 21:42:38 +0800

    Update can-script2.js

--- can-script2.js
+++ can-script2.js
@@ -1,3 +1,4 @@
+//0先获取到包含商品的json文件
 var promise=fetch("products.json");
 promise.then(
     function(response){
@@ -16,18 +17,62 @@ promise.then(
 function initial(products){
     let form=document.querySelector("form");
     let select=document.querySelector("select");
+    let searchTerm=document.querySelector("#searchTerm")
     let button=document.querySelector("button");
     var main=document.querySelector("main");
     //var products={};
-    var selectProducts=[];
+    var oneSelectProducts=[];
+    var twoSelectProducts=[];
+    selectOne(products);
     button.addEventListener("click", showSelectImg);
 
-    for(let i=0;i<products.length;i++){
-        showImg(products[i]);
+    //1先判断是什么类型的商品
+    function selectOne(products){
+        if(select.value==="all"){
+            oneSelectProducts=products;
+        }else{
+            for(let i=0;i<products.length;i++){
+                if(products[i].type===select.value.toLowerCase()){
+                    oneSelectProducts.push(products[i]);
+                }
+            }
+        }
+        selectTwo();
     }
 
+    //2再判断同类型中符合搜索关键字的商品
+    function selectTwo(){
+        let formatSearchTerm=searchTerm.value.toLowerCase().trim();
+        if(formatSearchTerm===""){
+            twoSelectProducts=oneSelectProducts;
+        }else{
+            if((oneSelectProducts.indexOf(formatSearchTerm)===-1)){
+                for(let i=0;i<oneSelectProducts.length;i++){
+                    twoSelectProducts.push(oneSelectProducts[i]);
+                }
+            }else{
+                let text=document.createTextNode("没有结果！");
+                main.appendChild(text);
+            }
+        }
+        for(let i=0;i<twoSelectProducts.length;i++){
+            fetchBlob(twoSelectProducts[i]);
+        }
+        
+    }
 
-    function showImg(product){
+    //获取商品图片地址
+    function fetchBlob(product){
+        let promise = fetch(product.image);
+        promise.then(response=>response.blob()).then(
+            blob=>{
+                let url=URL.createObjectURL(blob);
+                showImg(url,product);
+            }
+        ).catch(e=>console.log("图片获取失败："+e.message));
+    }
+
+    function showImg(url,product){
         let section=document.createElement("section");
         let h2=document.createElement("h2");
         let p=document.createElement("p");
@@ -35,7 +80,7 @@ function initial(products){
         section.setAttribute("class",product.type);
         h2.textContent=product.name;
         p.textContent="$"+product.price;
-        img.src="images/"+product.image;
+        img.src="images/"+url;
         img.setAttribute("alt",product.name);
         section.appendChild(h2);
         section.appendChild(p);
@@ -47,21 +92,21 @@ function initial(products){
     
    
     
-    function showSelectImg(){
-        event.preventDefault();
-        main.innerHTML="";
-        let key=select.value;
-        key=key.toLowerCase();
-        console.log(key);
-        if(key==="all"){
-            for(let i=0;i<products.length;i++)
-            {   showImg(products[i]);}
-        }else{
-            for(let i=0;i<products.length;i++){
-                if(key===products[i].type){
-                    showImg(products[i]);
-                }
-            }
-        }
-    }
+    // function showSelectImg(){
+    //     event.preventDefault();
+    //     main.innerHTML="";
+    //     let key=select.value;
+    //     key=key.toLowerCase();
+    //     console.log(key);
+    //     if(key==="all"){
+    //         for(let i=0;i<products.length;i++)
+    //         {   showImg(products[i]);}
+    //     }else{
+    //         for(let i=0;i<products.length;i++){
+    //             if(key===products[i].type){
+    //                 showImg(products[i]);
+    //             }
+    //         }
+    //     }
+    // }
 }
